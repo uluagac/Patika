27e79fee626cfd343e259e433e1954ee4fb553d7
uluagac <ersinuluagac@gmail.com>
2024-07-08 23:31:27 +0300

    static files uploaded

--- NodeJS/PCAT/app.js
+++ NodeJS/PCAT/app.js
@@ -1,13 +1,12 @@
 const express = require("express");
+const path = require("path")
 const app = express();
 
+// Middlewares
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description",
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
 });
 
 const port = 3000;
