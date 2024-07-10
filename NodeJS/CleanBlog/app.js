0e9900fc8e1faee11462b1b26383ceb7abc906d9
uluagac <ersinuluagac@gmail.com>
2024-07-10 21:19:17 +0300

    public middlewares and ejs modules

--- NodeJS/CleanBlog/app.js
+++ NodeJS/CleanBlog/app.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
 
+// Template Engine
+app.set("view engine", "ejs");
+
+// Middlewares
+app.use(express.static("public"));
+
+// Routes
 app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Blog Title",
-    description: "Blog description",
-  };
-  res.send(blog);
+  res.render("index");
 });
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.get("/post", (req, res) => {
+  res.render("post");
+});
+app.get("/add_post", (req, res) => {
+  res.render("add_post");
+});
+
 
 const port = 3000;
 app.listen(port, () => {
