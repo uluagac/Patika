105a8eb10c9ca4a8f1ca423048982941f1752d66
uluagac <ersinuluagac@gmail.com>
2024-07-14 17:27:19 +0300

    PCAT ve CleanBlog updated. mongoose and connection to db

--- NodeJS/CleanBlog/app.js
+++ NodeJS/CleanBlog/app.js
@@ -1,28 +1,42 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
+const path = require("path");
+const Post = require("./models/Post");
+
 const app = express();
+
+// Connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db");
 
 // Template Engine
 app.set("view engine", "ejs");
 
 // Middlewares
 app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 
 // Routes
-app.get("/", (req, res) => {
-  res.render("index");
+app.get("/", async (req, res) => {
+  const posts = await Post.find({})
+  res.render("index", {
+    posts
+  });
+  posts
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
-app.get("/post", (req, res) => {
-  res.render("post");
-});
 app.get("/add_post", (req, res) => {
   res.render("add_post");
 });
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body)
+  res.redirect("/");
+});
 
-
+// Server
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port}'de başladı!`);
