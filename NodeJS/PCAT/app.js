768d1b2edffc1dc11e5c54eb7c607c750c2216e1
uluagac <ersinuluagac@gmail.com>
2024-07-21 15:49:01 +0300

    delete photo button is active

--- NodeJS/PCAT/app.js
+++ NodeJS/PCAT/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
 const ejs = require("ejs");
 const path = require("path");
 const fs = require("fs");
@@ -19,6 +20,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(fileUpload());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 // Routes
 app.get("/", async (req, res) => {
@@ -27,27 +33,27 @@ app.get("/", async (req, res) => {
     photos,
   });
 });
+
 app.get("/photos/:id", async (req, res) => {
   const photo = await Photo.findById(req.params.id);
   res.render("photo", {
     photo,
   });
 });
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
 app.get("/add", (req, res) => {
   res.render("add");
 });
+
 app.post("/photos", async (req, res) => {
-  // console.log(req.files.image)
-  // await Photo.create(req.body);
-  // res.redirect("/");
   const uploadDir = "public/uploads";
   if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir);
   }
-
   let uploadedImage = req.files.image;
   let uploadPath = __dirname + "/public/uploads/" + uploadedImage.name;
   uploadedImage.mv(uploadPath, async () => {
@@ -59,6 +65,30 @@ app.post("/photos", async (req, res) => {
   });
 });
 
+app.get("/photos/edit/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  res.render("edit", {
+    photo,
+  });
+});
+
+app.put("/photos/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  photo.title = req.body.title;
+  photo.description = req.body.description;
+  photo.save();
+  res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete("/photos/:id", async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+  let deletedImage = __dirname + "/public" + photo.image;
+  fs.unlinkSync(deletedImage);
+  await Photo.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+// Port
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı!`);
