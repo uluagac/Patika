01a7c70f6d4a7e1cbe15975498fd7e346b6071b5
uluagac <ersinuluagac@gmail.com>
2024-07-27 17:19:29 +0300

    lesson 05

--- NodeJS/SmartEdu/app.js
+++ NodeJS/SmartEdu/app.js
@@ -1,17 +1,26 @@
 const express = require("express");
-const pageRoute = require("./routes/pageRoute")
+const mongoose = require("mongoose");
+const pageRoute = require("./routes/pageRoute");
+const courseRoute = require("./routes/courseRoute");
 
 const app = express();
+
+// CoonectDB
+mongoose.connect("mongodb://localhost/smartedu-db").then(() => {
+  console.log("DB Connected Successfuly");
+});
 
 // Template Engine
 app.set("view engine", "ejs");
 
 // Middlewares
 app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
-app.use('/', pageRoute);
-
+app.use("/", pageRoute);
+app.use("/courses", courseRoute);
 
 // Port
 const port = 3000;
