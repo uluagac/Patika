f2aa110067912478285bf9ca929aed6b82da02d2
uluagac <ersinuluagac@gmail.com>
2024-08-09 20:34:02 +0300

    created roles

--- NodeJS/SmartEdu/controllers/authController.js
+++ NodeJS/SmartEdu/controllers/authController.js
@@ -4,10 +4,7 @@ const User = require("../models/User");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      status: "success",
-      user,
-    });
+    res.status(201).redirect("/login");
   } catch (error) {
     res.status(400).json({
       status: "fail",
@@ -43,6 +40,6 @@ exports.getDashboardPage = async (req, res) => {
   const user = await User.findOne({ _id: req.session.userID });
   res.status(200).render("dashboard", {
     page_name: "dashboard",
-    user
+    user,
   });
 };
--- NodeJS/SmartEdu/models/User.js
+++ NodeJS/SmartEdu/models/User.js
@@ -16,6 +16,11 @@ const UserSchema = new Schema({
     type: String,
     require: true,
   },
+  role: {
+    type: String,
+    enum: ["student", "teacher", "admin"],
+    default: "student",
+  },
 });
 
 UserSchema.pre("save", function (next) {
