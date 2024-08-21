24330244491ec9090121c8eb9c2d1c12c051bac1
uluagac <ersinuluagac@gmail.com>
2024-08-21 22:34:32 +0300

    student course links

--- NodeJS/SmartEdu/controllers/authController.js
+++ NodeJS/SmartEdu/controllers/authController.js
@@ -20,10 +20,9 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
     let user = await User.findOne({ email });
     let same = await bcrypt.compare(password, user.password);
-    if (same) {
-      req.session.userID = user._id;
-      res.status(200).redirect("/users/dashboard");
-    }
+    //user sessions
+    req.session.userID = user._id;
+    res.status(200).redirect("/users/dashboard");
   } catch (error) {
     res.status(400).json({
       status: "fail",
@@ -39,7 +38,9 @@ exports.logoutUser = (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-  const user = await User.findOne({ _id: req.session.userID });
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    "courses"
+  );
   const categories = await Category.find();
   const courses = await Course.find({ user: req.session.userID });
   res.status(200).render("dashboard", {
